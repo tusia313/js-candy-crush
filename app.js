068b299b8322056cf853688b1d51e17a83b90c26
Marta Łoskot <loskotmarta@gmail.com>
2024-02-12 18:30:34 +0100

    Making colors

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const width = 8
 const squares = []
 
 const candyColors = [
-    'pink'
-    'yellow'
-    'purple'
-    'lightgreen'
-    'lightblue'
+    'pink',
+    'yellow',
+    'plum',
+    'lightgreen',
+    'lightblue',
     'orange'
 ]
 
@@ -20,6 +20,8 @@ const candyColors = [
 function createBoard() {
     for (let i=0; i < width*width; i++) {
         const square = document.createElement('div')
+        let randomColor = Math.floor(Math.random() * candyColors.length)
+        square.style.backgroundColor = candyColors[randomColor]
         grid.appendChild(square)
         squares.push(square)
     }
